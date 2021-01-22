7a59dec469080241816f6a509dc536e9b3a27f3a
thanhthiengcd18708 <66301633+thanhthiengcd18708@users.noreply.github.com>
2021-01-22 15:11:56 +0700

    Add project files.

---
+++ Events-Lab/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Events_Lab.MvcApplication" Language="C#" %>
---
+++ Events-Lab/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Events_Lab.Startup))]
+namespace Events_Lab
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
